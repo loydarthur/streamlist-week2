44ae0acecc6bfe48df7173212f69c2af286bacb2
loydarthur <loyd.arthur@student.uagc.edu>
2025-02-13 15:20:41 -0500

    Update
    Added nav bar and add/remove movie functionality.

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
+import StreamList from './components/StreamList';
+import Movies from './components/Movies';
+import Cart from './components/Cart';
+import About from './components/About';
+
+function App() {
+  return (
+    <Router>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<StreamList />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
